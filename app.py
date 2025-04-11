9fcad55a89d92c83ff5e42378f6b0346aa8b9880
kodaifukuda0311 <139470839+kodaifukuda0311@users.noreply.github.com>
2025-04-11 11:09:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ stopwords = nlp.Defaults.stop_words
 with open("tokenizer.pkl", "rb") as f:
     tokenizer = pickle.load(f)
 
-model = load_model("best_model.h5")
+model = load_model("best_model20250408.h5")
 
 # 時間帯エンコード
 def encode_hour_period(hour_str):
@@ -41,16 +41,16 @@ def predict_popularity(headline, hour_period):
 # Streamlit UI
 st.title("📰 Blueskyバズ予測ツール")
 
-st.markdown("### 📝 アプリの概要")
+st.markdown("#### 📝 アプリの概要")
 st.write("""
 これはあなたのBluesky投稿が「バズるかどうか」を予測するアプリです。 
-見出しと投稿時間帯を入力するだけで、AIが73%の精度でヒットの可能性を判定してくれます。
+見出しと投稿時間帯を入力するだけで、AIが74%の精度でヒットの可能性を判定してくれます。
 
-7～11時　→　morning
-12～14時　→ noon
-15～19時　→ afternoon
-20～23時　→ evening
-24～6時　→ midnight
+7～11時 →　morning
+12～14時 → noon
+15～19時 → afternoon
+20～23時 → evening
+24～6時 → midnight
 """)
 
 headline = st.text_input("記事の見出しを入力してください")
