37617c27ef07e8225a078b5d99f4945cb034724f
kodaifukuda0311 <139470839+kodaifukuda0311@users.noreply.github.com>
2025-04-23 17:58:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,36 +5,41 @@ import spacy
 import pickle
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+import os
 
-# NNモデル・Tokenizer・spacy.GiNZAの読み込み
-nlp = spacy.load("ja_ginza")
+@st.cache_resource
+def load_nlp():
+    model_name = "ja_core_news_lg"
+    if not spacy.util.is_package(model_name):
+        spacy.cli.download(model_name)
+    return spacy.load(model_name)
+
+@st.cache_resource
+def load_tokenizer():
+    with open("tokenizer.pkl", "rb") as f:
+        return pickle.load(f)
+
+@st.cache_resource
+def load_keras_model():
+    return load_model("best_model20250422.h5")
+
+nlp = load_nlp()
 stopwords = nlp.Defaults.stop_words
+tokenizer = load_tokenizer()
+model = load_keras_model()
 
-with open("tokenizer.pkl", "rb") as f:
-    tokenizer = pickle.load(f)
-
-model = load_model("best_model20250408.h5")
-
-# 時間帯エンコード
-def encode_hour_period(hour_str):
-    mapping = {"morning":0, "noon":1, "afternoon":2, "evening":3, "midnight":4}
-    one_hot = np.zeros(5)
-    one_hot[mapping[hour_str]] = 1
-    return one_hot
-
-# 前処理
-def preprocess(headline, hour_period):
+# Preprocess
+def preprocess(headline):
     doc = nlp(headline)
     tokens = [token.text for token in doc if token.pos_ in {"NOUN", "VERB", "ADJ", "X"} and token.text not in stopwords]
     text = " ".join(tokens)
     sequence = tokenizer.texts_to_sequences([text])
     padded = pad_sequences(sequence, maxlen=32)
-    hour_encoded = encode_hour_period(hour_period).reshape(1, -1)
-    return np.hstack([padded, hour_encoded])
+    return padded
 
-# 予測
-def predict_popularity(headline, hour_period):
-    X = preprocess(headline, hour_period)
+# Predictor 
+def predict_popularity(headline):
+    X = preprocess(headline)
     pred = model.predict(X)
     return "いいね！ヒットの可能性が高いです！" if pred[0][0] >= 0.5 else "ごめんね、ヒットの可能性は低いです…"
 
@@ -44,18 +49,12 @@ st.title("📰 Blueskyバズ予測ツール")
 st.markdown("#### 📝 アプリの概要")
 st.write("""
 これはあなたのBluesky投稿が「バズるかどうか」を予測するアプリです。 
-見出しと投稿時間帯を入力するだけで、AIが74%の精度でヒットの可能性を判定してくれます。
-
-7～11時 →　morning
-12～14時 → noon
-15～19時 → afternoon
-20～23時 → evening
-24～6時 → midnight
+見出しと投稿時間帯を入力するだけで、AIが74%の精度でヒットの可能性を判定してくれます。\\
+（モデルは20250422更新）
 """)
 
 headline = st.text_input("記事の見出しを入力してください")
-hour_period = st.selectbox("投稿時間帯を選んでください", ["morning", "noon", "afternoon", "evening", "midnight"])
 
 if st.button("予測する"):
-    result = predict_popularity(headline, hour_period)
+    result = predict_popularity(headline)
     st.success(result)
